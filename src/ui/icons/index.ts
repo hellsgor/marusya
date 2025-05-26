6e10f26a9d64bdbaf6e170066d3dd4133325611c
Igor Storchevoy <hellsgor@gmail.com>
2025-05-26 14:12:41 +0400

    Feat[icons]: added icons reexport

---
+++ src/ui/icons/index.ts
@@ -0,0 +1,7 @@
+export { ReactComponent as EmailIcon } from '../icons/email.svg';
+export { ReactComponent as MarusyaText } from '../icons/marusya-text.svg';
+export { ReactComponent as OkIcon } from '../icons/ok.svg';
+export { ReactComponent as SearchIcon } from '..//icons/search.svg';
+export { ReactComponent as TelegramIcon } from '../icons/telegram.svg';
+export { ReactComponent as VkIcon } from '../icons/vk.svg';
+export { ReactComponent as YouTubeIcon } from '../icons/youtube.svg';

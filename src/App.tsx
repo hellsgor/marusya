f147fb3efcd28d671ece832acdd8bdbca0f07de4
Igor Storchevoy <hellsgor@gmail.com>
2025-05-27 01:11:32 +0400

    Refactor[App]: removed MainLayout

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import './App.scss';
 
 import { BrowserRouter } from 'react-router';
-import { MainLayout } from './layouts/MainLayout/MainLayout';
 import { AppRotes } from './router/routes';
 
 function App() {
   return (
     <BrowserRouter>
-      <MainLayout>
-        <AppRotes />
-      </MainLayout>
+      <AppRotes />
     </BrowserRouter>
   );
 }

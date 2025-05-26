c7e117ee498711c9ec173875020d364e0867d0ef
Igor Storchevoy <hellsgor@gmail.com>
2025-05-26 22:41:03 +0400

    Feat[Footer]: created component

--- src/layouts/PublicLayout/PublicLayout.tsx
+++ src/layouts/PublicLayout/PublicLayout.tsx
@@ -1,12 +1,13 @@
 import type { ReactNode } from 'react';
 import { Header } from '../../components/Header/Header';
+import { Footer } from '../../components/Footer/Footer';
 
 export function PublicLayout({ children }: { children?: ReactNode }) {
   return (
     <>
       <Header />
       <main>{children}</main>
-      <footer></footer>
+      <Footer />
     </>
   );
 }

fbd52a6abd0be90624fc7f2ba122024bf88df34d
Igor Storchevoy <hellsgor@gmail.com>
2025-05-27 01:13:25 +0400

    Refactor[routes]: wrap route elements with MainLayout for consistent layout

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router';
 import { Home } from '../pages/Home';
 import { Genres } from '../pages/Genres';
 import { NoMatch } from '../pages/NoMatch';
+import { MainLayout } from '../layouts/MainLayout/MainLayout';
 
 export function AppRotes() {
   const navigationRoutes = [
@@ -13,7 +14,11 @@ export function AppRotes() {
   return (
     <Routes>
       {navigationRoutes.map((route) => (
-        <Route key={route.path} path={route.path} element={route.element} />
+        <Route
+          key={route.path}
+          path={route.path}
+          element={<MainLayout title={route.title}></MainLayout>}
+        />
       ))}
     </Routes>
   );

29450231b0f5e13aebb3248654e0515e4f795605
Igor Storchevoy <hellsgor@gmail.com>
2025-05-28 15:18:40 +0400

    Feat[MovieCard]: renamed FilmCard component to MovieCard

--- src/ui/MovieCard/MovieCard.tsx
+++ src/ui/MovieCard/MovieCard.tsx
@@ -1,34 +1,35 @@
+import st from './MovieCard.module.scss';
+
 import { memo } from 'react';
-import st from './FilmCard.module.scss';
 import { NavLink } from 'react-router';
 
-interface FilmCardProps {
+interface MovieCardProps {
   href: string;
   poster?: string;
   alt?: string;
   place?: number;
 }
 
-export const FilmCard = memo(function FilmCard({
+export const MovieCard = memo(function MovieCard({
   href,
   poster,
   alt,
   place,
-}: FilmCardProps) {
+}: MovieCardProps) {
   return (
-    <div className={st.filmCard}>
-      <NavLink to={href} className={st.filmCard__link}>
+    <div className={st.movieCard}>
+      <NavLink to={href} className={st.movieCard__link}>
         {poster && (
           <img
             src={poster}
             alt={alt}
             width={224}
             height={336}
-            className={st.filmCard__poster}
+            className={st.movieCard__poster}
           />
         )}
       </NavLink>
-      {place && <span className={st.filmCard__place}>{place}</span>}
+      {place && <span className={st.movieCard__place}>{place}</span>}
     </div>
   );
 });

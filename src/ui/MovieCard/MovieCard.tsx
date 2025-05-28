9b1afeb29c2e764d78e722fafdf56afb0657e35c
Igor Storchevoy <hellsgor@gmail.com>
2025-05-29 00:44:21 +0400

    Update[MovieCard]: added className property for customizing styles

--- src/ui/MovieCard/MovieCard.tsx
+++ src/ui/MovieCard/MovieCard.tsx
@@ -1,5 +1,7 @@
 import st from './MovieCard.module.scss';
 
+import clsx from 'clsx';
+
 import { memo } from 'react';
 import { NavLink } from 'react-router';
 
@@ -8,6 +10,7 @@ interface MovieCardProps {
   poster?: string;
   alt?: string;
   place?: number;
+  className?: string;
 }
 
 export const MovieCard = memo(function MovieCard({
@@ -15,18 +18,13 @@ export const MovieCard = memo(function MovieCard({
   poster,
   alt,
   place,
+  className,
 }: MovieCardProps) {
   return (
-    <div className={st.movieCard}>
+    <div className={clsx(st.movieCard, className)}>
       <NavLink to={href} className={st.movieCard__link}>
         {poster && (
-          <img
-            src={poster}
-            alt={alt}
-            width={224}
-            height={336}
-            className={st.movieCard__poster}
-          />
+          <img src={poster} alt={alt} className={st.movieCard__poster} />
         )}
       </NavLink>
       {place && <span className={st.movieCard__place}>{place}</span>}

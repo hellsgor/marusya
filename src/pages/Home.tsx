df5c34ec89f6ff20dc5aa8b6dd49a07428dbfeac
Igor Storchevoy <hellsgor@gmail.com>
2025-05-30 14:27:12 +0400

    Update[Home]: added Section with MoviesTopList

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,22 @@
+import { useQuery } from '@tanstack/react-query';
 import { MoviesTopList } from '../ui/MoviesTopList/MoviesTopList';
+import type { MoviesModel } from '../models/Movie';
+import { moviesService } from '../api/moviesService';
+import { Section } from '../ui/Section/Section';
 
 export function Home() {
+  const top10Query = useQuery<MoviesModel | undefined>({
+    queryKey: ['movies', 'top10'],
+    queryFn: moviesService.getTop10,
+    staleTime: Infinity,
+  });
+
   return (
     <>
-      <MoviesTopList />
+      <Section>
+        <h2 className={'heading heading_2'}>Топ 10 фильмов</h2>
+        <MoviesTopList query={top10Query} />
+      </Section>
     </>
   );
 }

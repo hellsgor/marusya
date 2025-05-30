894d38cfeb0a4fc892d95264dad46db567a72255
Igor Storchevoy <hellsgor@gmail.com>
2025-05-30 14:21:43 +0400

    Refactoring[MoviesTopList]: simplify component structure

--- src/ui/MoviesTopList/MoviesTopList.tsx
+++ src/ui/MoviesTopList/MoviesTopList.tsx
@@ -1,45 +1,37 @@
 import st from './MoviesTopList.module.scss';
-import { useQuery } from '@tanstack/react-query';
-import { moviesService } from '../../api/moviesService';
-import type { MoviesModel } from '../../models/Movie';
 import { MovieCard } from '../MovieCard/MovieCard';
-import { Container } from '../Container/Container';
 import { Loader } from '../Loader/Loader';
+import type { UseQueryResult } from '@tanstack/react-query';
+import type { MoviesModel } from '../../models/Movie';
 
-export function MoviesTopList() {
-  const { data, isPending, isError } = useQuery<MoviesModel | undefined>({
-    queryKey: ['movies', 'top10'],
-    queryFn: moviesService.getTop10,
-    staleTime: Infinity,
-  });
+interface MoviesTopListProps {
+  query: UseQueryResult<MoviesModel | undefined>;
+}
+
+export function MoviesTopList({ query }: MoviesTopListProps) {
+  const { data, isPending, isError } = query;
 
   return (
     <div className={st.moviesTopList}>
-      <Container>
-        <div className={st.moviesTopList__wrapper}>
-          <h2 className={'heading heading_2'}>Топ 10 фильмов</h2>
+      {isPending && <Loader />}
 
-          {isPending && <Loader />}
+      {isError && (
+        <span>Что-то пошло не так. Попробуйте перезагрузить страницу.</span>
+      )}
 
-          {isError && (
-            <span>Что-то пошло не так. Попробуйте перезагрузить страницу.</span>
-          )}
-
-          <div className={st.moviesTopList__list}>
-            {data &&
-              data.map((movie, index) => (
-                <MovieCard
-                  key={movie.id}
-                  href={`/${movie.id}`}
-                  poster={movie.posterUrl || undefined}
-                  alt={`${movie.title} poster`}
-                  place={index + 1}
-                  className={st.moviesTopList__item}
-                />
-              ))}
-          </div>
-        </div>
-      </Container>
+      <div className={st.moviesTopList__list}>
+        {data &&
+          data.map((movie, index) => (
+            <MovieCard
+              key={movie.id}
+              href={`/${movie.id}`}
+              poster={movie.posterUrl || undefined}
+              alt={`${movie.title} poster`}
+              place={index + 1}
+              className={st.moviesTopList__item}
+            />
+          ))}
+      </div>
     </div>
   );
 }

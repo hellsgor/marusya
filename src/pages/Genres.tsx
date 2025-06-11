1d40ef20d8f0dcf669ea1059ed06b91715118174
Igor Storchevoy <hellsgor@gmail.com>
2025-06-11 18:07:07 +0400

    Update[Genres]: added section and heading to page component

--- src/pages/Genres.tsx
+++ src/pages/Genres.tsx
@@ -1,3 +1,9 @@
+import { Section } from '../ui/Section/Section';
+
 export function Genres() {
-  return <div style={{ color: 'white' }}>i am genres page</div>;
+  return (
+    <Section indentsClasses="pt-64 pb-160 pt-md-16 pb-md-40">
+      <h1 className="heading heading_1">Жанры фильмов</h1>
+    </Section>
+  );
 }

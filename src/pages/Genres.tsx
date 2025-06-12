c0991d78dae80e8f7a17ea5911f499e6eb9e975c
Igor Storchevoy <hellsgor@gmail.com>
2025-06-13 01:01:56 +0400

    Feat[GenresList]: added component for displaying list of genres

--- src/pages/Genres.tsx
+++ src/pages/Genres.tsx
@@ -1,9 +1,11 @@
+import { GenresList } from '../components/GenresList/GenresList';
 import { Section } from '../ui/Section/Section';
 
 export function Genres() {
   return (
     <Section indentsClasses="pt-64 pb-160 pt-md-16 pb-md-40">
       <h1 className="heading heading_1">Жанры фильмов</h1>
+      <GenresList />
     </Section>
   );
 }

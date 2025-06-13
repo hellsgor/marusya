9278b79fe6da69f488222e8942faa519ef729cf7
Igor Storchevoy <hellsgor@gmail.com>
2025-06-13 23:54:50 +0400

    Update[MainLayout]: updated to handle dynamic title based on route parameters

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.scss';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import { BrowserRouter } from 'react-router';
-import { AppRotes } from './router/routes';
+import { AppRoutes } from './router/routes';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from './api/queryClient';
 
@@ -11,7 +11,7 @@ function App() {
   return (
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
-        <AppRotes />
+        <AppRoutes />
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </BrowserRouter>
--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -4,12 +4,18 @@ import { Genres } from '../pages/Genres';
 import { NoMatch } from '../pages/NoMatch';
 import { MainLayout } from '../layouts/MainLayout/MainLayout';
 import { Genre } from '../pages/Genre';
+import { GENRES_RU } from '../constants';
+import { getTitle } from '../helpers';
 
-export function AppRotes() {
+export function AppRoutes() {
   const navigationRoutes = [
     { path: '/', element: <Home />, title: 'Главная' },
     { path: '/genres', element: <Genres />, title: 'Жанры' },
-    { path: '/genres/:genreName', element: <Genre /> },
+    {
+      path: '/genres/:genreName',
+      element: <Genre />,
+      title: (str: string) => getTitle(str, GENRES_RU),
+    },
     { path: '*', element: <NoMatch />, title: '404' },
   ];
 
@@ -19,9 +25,7 @@ export function AppRotes() {
         <Route
           key={route.path}
           path={route.path}
-          element={
-            <MainLayout title={route.title ?? ''}>{route.element}</MainLayout>
-          }
+          element={<MainLayout title={route.title}>{route.element}</MainLayout>}
         />
       ))}
     </Routes>

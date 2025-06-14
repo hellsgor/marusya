7a4efba534ab7a456a5ba9f095b35ed24b26c373
Igor Storchevoy <hellsgor@gmail.com>
2025-06-15 01:57:32 +0400

    Feat[useMovies]: added hook for getting movies with infinite scroll support

---
+++ src/hooks/useMovies.ts
@@ -0,0 +1,27 @@
+import type { GenreModel } from '../models';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { moviesService } from '../api/moviesService';
+import { MOVIES_PER_PAGE } from '../constants';
+
+export function useMovies(genreName: GenreModel['name'] | undefined) {
+  const query = useInfiniteQuery({
+    queryKey: ['movies', genreName],
+    queryFn: ({ pageParam = 1 }) =>
+      moviesService.getMovies({
+        genre: genreName,
+        count: MOVIES_PER_PAGE,
+        page: pageParam,
+      }),
+    initialPageParam: 1,
+    getNextPageParam: (last, all) =>
+      last.length === MOVIES_PER_PAGE ? all.length + 1 : undefined,
+  });
+
+  const allMovies = query.data?.pages.flat() ?? [];
+  const sorted = allMovies.toSorted((a, b) => b.tmdbRating - a.tmdbRating);
+
+  return {
+    movies: sorted,
+    query,
+  };
+}

075f4f2469f989020a7cc28b1e779eb80c2bb3f7
Igor Storchevoy <hellsgor@gmail.com>
2025-06-14 23:34:51 +0400

    Update[Genre]: used BackTitleBar for heading display

--- src/pages/Genre.tsx
+++ src/pages/Genre.tsx
@@ -2,14 +2,20 @@ import { useParams } from 'react-router';
 import { Section } from '../ui/Section/Section';
 import { getTitle } from '../helpers';
 import { GENRES_RU } from '../constants';
+import { BackTitleBar } from '../ui/BackTitleBar/BackTitleBar';
 
 export function Genre() {
   const { genreName } = useParams();
+
   return (
     <Section indentsClasses="pt-64 pb-160 pt-md-16 pb-md-40">
-      <h1 className="heading heading_1">
-        {getTitle(genreName ?? '', GENRES_RU)}
-      </h1>
+      <BackTitleBar
+        headingNode={
+          <h1 className="heading heading_1">
+            {getTitle(genreName ?? '', GENRES_RU)}
+          </h1>
+        }
+      />
     </Section>
   );
 }

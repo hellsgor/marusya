781914ad6def56a42279c1baa164a25639b49bac
Igor Storchevoy <hellsgor@gmail.com>
2025-06-14 23:02:15 +0400

    Feat[Genre]: created  genre page

---
+++ src/pages/Genre.tsx
@@ -0,0 +1,15 @@
+import { useParams } from 'react-router';
+import { Section } from '../ui/Section/Section';
+import { getTitle } from '../helpers';
+import { GENRES_RU } from '../constants';
+
+export function Genre() {
+  const { genreName } = useParams();
+  return (
+    <Section indentsClasses="pt-64 pb-160 pt-md-16 pb-md-40">
+      <h1 className="heading heading_1">
+        {getTitle(genreName ?? '', GENRES_RU)}
+      </h1>
+    </Section>
+  );
+}

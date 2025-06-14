40946d89c934da607a14f64c0250cd4a17abd853
Igor Storchevoy <hellsgor@gmail.com>
2025-06-15 01:58:28 +0400

    Feat[Genre]: added functionality for loading next page of movies with the "Show more" button

--- src/pages/Genre.tsx
+++ src/pages/Genre.tsx
@@ -3,9 +3,17 @@ import { Section } from '../ui/Section/Section';
 import { getTitle } from '../helpers';
 import { GENRES_RU } from '../constants';
 import { BackTitleBar } from '../ui/BackTitleBar/BackTitleBar';
+import { Button } from '../ui/Button/Button';
+import { useMovies } from '../hooks/useMovies';
 
 export function Genre() {
   const { genreName } = useParams();
+  const {
+    movies,
+    query: { fetchNextPage, hasNextPage },
+  } = useMovies(genreName);
+
+  console.log(movies);
 
   return (
     <Section indentsClasses="pt-64 pb-160 pt-md-16 pb-md-40">
@@ -16,6 +24,15 @@ export function Genre() {
           </h1>
         }
       />
+
+      <Button
+        variant="primary"
+        onClick={() => fetchNextPage()}
+        disabled={!hasNextPage}
+        style={{ margin: '0 auto' }}
+      >
+        Показать ещё
+      </Button>
     </Section>
   );
 }

991f0f4bf26fab58eaa91d28271d706fc88a9bf2
Igor Storchevoy <hellsgor@gmail.com>
2025-06-16 17:44:46 +0400

    Feat[moviePropertiesRu]: added constant with translation of movie properties into Russian

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -5,3 +5,4 @@ export { BASE_URL } from './baseUrl';
 export { ERRORS_TEXTS } from './errorsTexts';
 export { GENRES_RU } from './genresRu';
 export { MOVIES_PER_PAGE } from './moviesPerPage';
+export { MOVIE_PROPERTIES_RU } from './moviePropertiesRu';

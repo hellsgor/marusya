4f300b25b40549a598893000de1fcae5db69d4b0
Igor Storchevoy <hellsgor@gmail.com>
2025-07-15 23:29:50 +0400

    Update[MovieDetails]: added isSmall prop & modifier

--- src/ui/MovieDetails/MovieDetails.tsx
+++ src/ui/MovieDetails/MovieDetails.tsx
@@ -2,21 +2,25 @@ import st from './MovieDetails.module.scss';
 import type { MovieModel } from '../../models';
 import { formatRuntime } from '../../helpers';
 import { Rating } from '../Rating/Rating';
+import clsx from 'clsx';
 
 export type MovieDetailsProps = Pick<
   MovieModel,
   'tmdbRating' | 'releaseYear' | 'genres' | 'runtime'
->;
+> & {
+  isSmall?: boolean;
+};
 
 export function MovieDetails({
   tmdbRating,
   releaseYear,
   genres,
   runtime,
+  isSmall,
 }: MovieDetailsProps) {
   return (
-    <div className={st.movieDetails}>
-      <Rating rate={tmdbRating} />
+    <div className={clsx(st.movieDetails, isSmall && st.movieDetails_small)}>
+      <Rating rate={tmdbRating} isSmall={isSmall} />
       <span>{releaseYear}</span>
       {genres.map((genre, idx) => (
         <span key={idx}>{genre}</span>

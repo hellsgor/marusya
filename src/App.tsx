8f019d9fe07a8abdb73c3220d5ef780cb04c0c46
Igor Storchevoy <hellsgor@gmail.com>
2025-07-23 10:58:09 +0400

    Feat[store]: added Redux store and integrated it to App

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,21 @@ import { AppRoutes } from './router/routes';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from './api/queryClient';
 import { ScrollToTop } from './components/ScrollToTop/ScrollToTop';
+import { store } from './store';
+import { Provider } from 'react-redux';
 
 function App() {
   return (
-    <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <ScrollToTop />
-        <AppRoutes />
-        <div id="modals"></div>
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <ScrollToTop />
+          <AppRoutes />
+          <div id="modals"></div>
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+      </BrowserRouter>
+    </Provider>
   );
 }
 

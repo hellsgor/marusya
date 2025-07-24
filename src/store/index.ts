a21998a27537ebcca4a0fa878a1de9e1ecfe64b0
Igor Storchevoy <hellsgor@gmail.com>
2025-07-24 08:40:06 +0400

    Feat[authModalSlice]: created authModalSlice

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { authReducer } from './authSlice';
+import { authModalReducer } from './authModalSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    authModal: authModalReducer,
   },
 });
 

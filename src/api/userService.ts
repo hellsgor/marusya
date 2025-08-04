7f44522d6fd92b85c14849d2a997e8cd186295b8
Igor Storchevoy <hellsgor@gmail.com>
2025-08-04 22:59:05 +0400

    Feat[userService]: added getProfile method

--- src/api/userService.ts
+++ src/api/userService.ts
@@ -1,3 +1,4 @@
+import { ProfileSchema } from '../models';
 import { SuccessLoginSchema, type LoginModel } from '../models/User';
 import { apiClient } from './apiClient';
 
@@ -7,6 +8,14 @@ export const userService = {
       method: 'POST',
       url: '/auth/login',
       data,
+      withCredentials: true,
+    });
+  },
+  getProfile: async () => {
+    return apiClient(ProfileSchema, {
+      method: 'GET',
+      url: '/profile',
+      withCredentials: true,
     });
   },
 };

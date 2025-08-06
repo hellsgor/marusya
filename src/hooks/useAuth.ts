6d50b746d677643fdbecb0161a00271bb54a67e8
Igor Storchevoy <hellsgor@gmail.com>
2025-08-06 10:34:00 +0400

    Update[useAuth]: added error typing

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,10 +1,11 @@
 import { useMutation } from '@tanstack/react-query';
-import type { LoginModel } from '../models/User';
+import type { LoginModel, SuccessLoginModel } from '../models/User';
 import { userService } from '../api/userService';
+import type { AxiosError } from 'axios';
 
 export function useAuth(onSuccessReset: () => void) {
-  return useMutation({
-    mutationFn: (data: LoginModel) => userService.login(data),
+  return useMutation<SuccessLoginModel, AxiosError, LoginModel>({
+    mutationFn: (data) => userService.login(data),
     onSuccess: (data) => {
       if (data.result) {
         onSuccessReset();

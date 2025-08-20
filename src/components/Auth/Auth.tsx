b83e61783bda2b1d27a77f97d08f9309447655d5
Igor Storchevoy <hellsgor@gmail.com>
2025-08-21 00:00:17 +0400

    Update[Auth]: use created modals

--- src/components/Auth/Auth.tsx
+++ src/components/Auth/Auth.tsx
@@ -1,12 +1,10 @@
 import { useProfile } from '../../hooks/useProfile';
-import { closeModal, openModal } from '../../store/authModalSlice';
+import { openModal } from '../../store/authModalSlice';
 import { useAppDispatch, useAppSelector } from '../../store/hooks';
-import { Button } from '../../ui/Button/Button';
 import { User } from '../../ui/icons';
 import { MenuItem } from '../../ui/MenuItem/MenuItem';
-import { Modal } from '../../ui/Modal/Modal';
-import { AuthForm } from '../AuthForm/AuthForm';
-import { RegistrationForm } from '../Form/RegistrationForm';
+import { LoginModal } from '../LoginModal/LoginModal';
+import { RegisterModal } from '../RegisterModal/RegisterModal';
 import { ThanksModal } from '../ThanksModal/ThanksModal';
 
 interface AuthProps {
@@ -15,26 +13,18 @@ interface AuthProps {
 
 export function Auth({ isVertTablet }: AuthProps) {
   const { isLoggedIn, user } = useAppSelector((state) => state.auth);
-  const currentAuthModal = useAppSelector((state) => state.authModal);
-
   const dispatch = useAppDispatch();
 
   useProfile();
 
-  const toggleModals = () => {
-    dispatch(openModal(currentAuthModal === 'register' ? 'login' : 'register'));
-  };
-
   return (
     <>
-      {isLoggedIn && (
+      {isLoggedIn ? (
         <MenuItem
           href="/account"
           children={isVertTablet ? <User /> : user?.name}
         />
-      )}
-
-      {!isLoggedIn && (
+      ) : (
         <>
           <MenuItem
             onClick={() => {
@@ -43,37 +33,8 @@ export function Auth({ isVertTablet }: AuthProps) {
             children={isVertTablet ? <User /> : 'Войти'}
           />
 
-          <Modal
-            isVisible={
-              currentAuthModal === 'login' || currentAuthModal === 'register'
-            }
-            onClose={() => {
-              dispatch(closeModal());
-            }}
-          >
-            {currentAuthModal === 'login' && (
-              <AuthForm
-                afterSuccess={() => {
-                  dispatch(closeModal());
-                }}
-              />
-            )}
-            {currentAuthModal === 'register' && (
-              <>
-                <h3 className="heading heading_3">Регистрация</h3>
-                <RegistrationForm
-                  afterSuccess={() => {
-                    dispatch(openModal('thanks'));
-                  }}
-                />
-              </>
-            )}
-
-            <Button variant="ghost" onClick={toggleModals}>
-              {currentAuthModal === 'login' && 'Регистрация'}
-              {currentAuthModal === 'register' && 'У меня уже есть аккаунт'}
-            </Button>
-          </Modal>
+          <LoginModal />
+          <RegisterModal />
           <ThanksModal />
         </>
       )}

46e17b7e7cbb28cbba7a4136f45e5e4a0ec31dc9
Igor Storchevoy <hellsgor@gmail.com>
2025-08-22 23:53:25 +0400

    Fix[useAuth]: fixed success type

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,10 +1,10 @@
 import { useMutation } from '@tanstack/react-query';
-import type { LoginModel, SuccessModel } from '../models';
+import type { LoginModel, SuccessLoginModel } from '../models';
 import { userService } from '../api/userService';
 import type { AxiosError } from 'axios';
 
 export function useAuth(onSuccessReset: () => void) {
-  return useMutation<SuccessModel, AxiosError, LoginModel>({
+  return useMutation<SuccessLoginModel, AxiosError, LoginModel>({
     mutationFn: (data) => userService.login(data),
     onSuccess: (data) => {
       if (data.result) {

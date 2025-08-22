0348accbc01466895e89bcfc7aa3ab9ef224871c
Igor Storchevoy <hellsgor@gmail.com>
2025-08-22 23:52:04 +0400

    Fix[useProfile]: updated query params

--- src/hooks/useProfile.ts
+++ src/hooks/useProfile.ts
@@ -1,23 +1,18 @@
 import { useQuery } from '@tanstack/react-query';
 import { userService } from '../api/userService';
 import type { ProfileModel } from '../models';
-import { useAppDispatch, useAppSelector } from '../store/hooks';
+import { useAppDispatch } from '../store/hooks';
 import { setUser } from '../store/authSlice';
 import { queryClient } from '../api/queryClient';
 import { useEffect } from 'react';
 
 export function useProfile() {
-  const { isLoggedIn } = useAppSelector((state) => state.auth);
   const dispatch = useAppDispatch();
 
   const query = useQuery<ProfileModel>({
     queryKey: ['profile'],
-    refetchOnWindowFocus: false,
-    refetchOnMount: false,
-    refetchOnReconnect: false,
     queryFn: () => userService.get(),
     retry: false,
-    enabled: isLoggedIn,
   });
 
   useEffect(() => {

2843a17ff48e11847a83ec103229b349452006ff
Igor Storchevoy <hellsgor@gmail.com>
2025-08-25 17:06:28 +0400

    Update[Profile]: added heading

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,3 +1,9 @@
+import { Section } from '../ui/Section/Section';
+
 export function Profile() {
-  return <p>Страница профиля</p>;
+  return (
+    <Section indentsClasses="pt-64 pb-160 pt-md-16 pb-md-40">
+      <h1>Мой аккаунт</h1>
+    </Section>
+  );
 }

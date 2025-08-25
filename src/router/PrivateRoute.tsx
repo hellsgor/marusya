4b883f229f1025e1322f229e0881cb1fbc7e5666
Igor Storchevoy <hellsgor@gmail.com>
2025-08-25 17:27:49 +0400

    Fix[PrivateRoute]: removed state

--- src/router/PrivateRoute.tsx
+++ src/router/PrivateRoute.tsx
@@ -1,26 +1,14 @@
 import type { ReactElement } from 'react';
 import { useAppSelector } from '../store/hooks';
-import { Navigate, useLocation } from 'react-router';
+import { Navigate } from 'react-router';
 import { Loader } from '../ui/Loader/Loader';
 
 export function PrivateRoute({ children }: { children: ReactElement }) {
   const { status } = useAppSelector((state) => state.auth);
 
-  const location = useLocation();
-
   if (status === 'loading') return <Loader size="big" />;
 
-  if (status === 'unauthenticated') {
-    return (
-      <Navigate
-        to="/"
-        replace
-        state={{
-          from: { pathname: location.pathname, search: location.search },
-        }}
-      />
-    );
-  }
+  if (status === 'unauthenticated') return <Navigate to="/" replace />;
 
   return children;
 }

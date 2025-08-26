14d77896bd592aa1d922d3a73ab4f9f35f8c9eb1
Igor Storchevoy <hellsgor@gmail.com>
2025-08-27 00:05:00 +0400

    Update[Favorites]: displayed list of movies, error and loader

--- src/components/Favorites/Favorites.tsx
+++ src/components/Favorites/Favorites.tsx
@@ -1,8 +1,13 @@
 import { useFavorites } from '../../hooks/useFavorites';
+import { ErrorText } from '../../ui/ErrorText/ErrorText';
+import { Loader } from '../../ui/Loader/Loader';
+import { MoviesList } from '../../ui/MoviesList/MoviesList';
 
 export function Favorites() {
-  const fav = useFavorites();
-  console.log(fav);
+  const { isFetching, data, isError } = useFavorites();
 
-  return <p>Избранное</p>;
+  if (isFetching) return <Loader size="big" />;
+  if (!data || isError) return <ErrorText errorKey={'e001'} />;
+
+  return <MoviesList data={data} isSlider={true} isFavorites={true} />;
 }

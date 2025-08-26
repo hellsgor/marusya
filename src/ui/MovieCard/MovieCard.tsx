c647f25ea966b74dc990d294a6f4d4aa20bc7137
Igor Storchevoy <hellsgor@gmail.com>
2025-08-27 00:02:45 +0400

    Update[MovieCard]: added remove from favorites button

--- src/ui/MovieCard/MovieCard.tsx
+++ src/ui/MovieCard/MovieCard.tsx
@@ -5,22 +5,27 @@ import clsx from 'clsx';
 import { memo } from 'react';
 import { NavLink } from 'react-router';
 import { Poster } from '../Poster/Poster';
+import { CloseButton } from '../CloseButton/CloseButton';
 
 interface MovieCardProps {
+  id: number;
   href: string;
   title: string;
   poster?: string;
   alt?: string;
   place?: number;
+  removeButton?: boolean;
   className?: string;
 }
 
 export const MovieCard = memo(function MovieCard({
+  id,
   href,
   title,
   poster,
   alt,
   place,
+  removeButton,
   className,
 }: MovieCardProps) {
   return (
@@ -33,6 +38,12 @@ export const MovieCard = memo(function MovieCard({
         <Poster src={poster} alt={alt} />
       </NavLink>
       {place && <span className={st.movieCard__place}>{place}</span>}
+      {removeButton && (
+        <CloseButton
+          onClick={() => console.log(id)}
+          className={st.movieCard__removeButton}
+        />
+      )}
     </div>
   );
 });

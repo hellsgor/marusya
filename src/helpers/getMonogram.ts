79f7228f04cb425864d8724763f6a438a99cf210
Igor Storchevoy <hellsgor@gmail.com>
2025-08-27 22:08:54 +0400

    Feat[getMonogram]: created function

---
+++ src/helpers/getMonogram.ts
@@ -0,0 +1,3 @@
+export function getMonogram(strings: string[]): string {
+  return strings.map((str) => str[0].toUpperCase() ?? '').join('');
+}
--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -2,3 +2,4 @@ export { getErrorText } from './getErrorText';
 export { formatRuntime } from './formatRuntime';
 export { formatFirstLetter } from './formatFirstLetter';
 export { getTitle } from './getTitle';
+export { getMonogram } from './getMonogram';

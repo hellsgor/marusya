ea7f86e5a159ca73a69890d0063d3da92e2cea24
Igor Storchevoy <hellsgor@gmail.com>
2025-08-27 22:19:55 +0400

    Update[Settings]: added user data

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -1,3 +1,42 @@
+import { getMonogram } from '../../helpers';
+import { useAppSelector } from '../../store/hooks';
+import { EmailIcon } from '../../ui/icons';
+import { Loader } from '../../ui/Loader/Loader';
+import { ProfileInfo } from '../../ui/ProfileInfo/ProfileInfo';
+import st from './Settings.module.scss';
+
 export function Settings() {
-  return <p>настройки</p>;
+  const { user, status } = useAppSelector((state) => state.auth);
+
+  if (status !== 'authenticated') {
+    return <Loader size="big" />;
+  }
+
+  const info = [
+    {
+      label: 'Имя Фамилия',
+      content: `${user?.name} ${user?.surname}`,
+      markerContent: (
+        <span>{getMonogram([user?.name ?? '', user?.surname ?? ''])}</span>
+      ),
+    },
+
+    {
+      label: 'Электронная почта',
+      content: `${user?.email}`,
+      markerContent: <EmailIcon />,
+    },
+  ];
+
+  return (
+    <div className={st.settings}>
+      <ul className={st.settings__info}>
+        {info.map((item) => (
+          <li key={item.label}>
+            <ProfileInfo {...item} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }

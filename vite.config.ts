efadf97b96bf0e6a2c8079cc471490bb2d508c6f
Igor Storchevoy <hellsgor@gmail.com>
2025-09-03 13:45:14 +0400

    Chore: implemented src alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import svgr from 'vite-plugin-svgr';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
-// https://vite.dev/config/
 export default defineConfig({
   server: {
     host: true,
   },
-  plugins: [
-    svgr({
-      include: '**/*.svg',
-      svgrOptions: { exportType: 'named' },
-    }),
-    react(),
-  ],
+  plugins: [react(), tsconfigPaths()],
 });

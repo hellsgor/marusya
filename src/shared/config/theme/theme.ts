4f30a3da791f595adcd61f2bc7cc7c38a946f11f
Igor Storchevoy <hellsgor@gmail.com>
2025-09-04 00:41:23 +0400

    Feat[shared/theme]: created breakpoints constant and added to theme

--- src/shared/config/theme/theme.ts
+++ src/shared/config/theme/theme.ts
@@ -1,9 +1,11 @@
+import { BREAKPOINTS } from './breakpoints';
 import { palette } from './palette';
 import { transition } from './transition';
 
 export const theme = {
   colors: palette,
   transition,
+  breakpoints: BREAKPOINTS,
 } as const;
 
 export type AppTheme = typeof theme;

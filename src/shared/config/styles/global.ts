41460319488cc4ccbd45ffe612045adfbe47f3c3
Igor Storchevoy <hellsgor@gmail.com>
2025-09-03 22:40:59 +0400

    Feat[shared/styles]: added fonts

--- src/shared/config/styles/global.ts
+++ src/shared/config/styles/global.ts
@@ -2,9 +2,11 @@ import { createGlobalStyle } from 'styled-components';
 import { reset } from './reset';
 import normalize from 'styled-normalize';
 import { bodyBg } from './bodyBg';
+import { fonts } from './fonts';
 
 export const GlobalStyles = createGlobalStyle`
   ${normalize}
   ${reset}
   ${bodyBg}
+  ${fonts}
 `;

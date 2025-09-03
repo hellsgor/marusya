fa13c9e8c10ca7a0a66a7bda34e59071c410b93d
Igor Storchevoy <hellsgor@gmail.com>
2025-09-03 13:15:59 +0400

    Ref: added entrypoint and App component

---
+++ src/app/App.tsx
@@ -0,0 +1,32 @@
+// import './App.scss';
+
+// import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+// import { BrowserRouter } from 'react-router';
+// import { AppRoutes } from './router/routes';
+// import { QueryClientProvider } from '@tanstack/react-query';
+// import { queryClient } from './api/queryClient';
+// import { ScrollToTop } from './components/ScrollToTop/ScrollToTop';
+// import { store } from './store';
+// import { Provider } from 'react-redux';
+
+// function App() {
+//   return (
+//     <Provider store={store}>
+//       <BrowserRouter>
+//         <QueryClientProvider client={queryClient}>
+//           <ScrollToTop />
+//           <AppRoutes />
+//           <div id="modals"></div>
+//           <ReactQueryDevtools initialIsOpen={false} />
+//         </QueryClientProvider>
+//       </BrowserRouter>
+//     </Provider>
+//   );
+// }
+
+function App() {
+  return <div>Hello World</div>;
+}
+
+export default App;

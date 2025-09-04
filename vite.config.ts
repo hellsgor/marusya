5a6acdc3d6149f7b70977604f9aca5aaff82a1a9
Igor Storchevoy <hellsgor@gmail.com>
2025-09-04 12:41:18 +0400

    Update[icon]: configured vite with svgr plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   server: {
     host: true,
   },
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), svgr(), tsconfigPaths()],
 });

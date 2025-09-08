0552ef3364ee4c7e09a77d457f482d454792b309
Igor Storchevoy <hellsgor@gmail.com>
2025-09-08 23:25:48 +0400

    Update[widgets/header]: removed using useLocation & isActive props from MainMenu component

--- src/widgets/header/ui/main-menu/MainMenu.tsx
+++ src/widgets/header/ui/main-menu/MainMenu.tsx
@@ -1,19 +1,14 @@
 import { MenuItem } from '@/shared/ui';
 import { NAV_ITEMS } from './MainMenu.constants';
-import { useLocation } from 'react-router';
 import * as S from './MainMenu.styled';
 
 export function MainMenu() {
-  const { pathname } = useLocation();
-
   return (
     <nav>
       <S.StyledNavList>
         {Object.values(NAV_ITEMS).map((item) => (
           <li key={item.path}>
-            <MenuItem href={item.path} isActive={pathname === item.path}>
-              {item.text}
-            </MenuItem>
+            <MenuItem href={item.path}>{item.text}</MenuItem>
           </li>
         ))}
       </S.StyledNavList>

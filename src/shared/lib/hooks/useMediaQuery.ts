87cf3dc9777187a88bcabd84dffa083d8b1439c8
Igor Storchevoy <hellsgor@gmail.com>
2025-09-08 23:54:05 +0400

    Feat[shared/lib]: created useMediaQuery

---
+++ src/shared/lib/hooks/useMediaQuery.ts
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react';
+import type { BREAKPOINTS } from '@/shared/config/theme/breakpoints';
+
+export function useMediaQuery(
+  bp: (typeof BREAKPOINTS)[keyof typeof BREAKPOINTS],
+  isMax: boolean = true,
+): boolean {
+  const query = `(${isMax ? 'max' : 'min'}-width: ${isMax ? bp - 1 : bp}px)`;
+
+  const [matches, setMatches] = useState(
+    () => window.matchMedia(query).matches,
+  );
+
+  useEffect(() => {
+    const mql = window.matchMedia(query);
+    const handler = (e: MediaQueryListEvent) => setMatches(e.matches);
+
+    mql.addEventListener('change', handler);
+
+    return () => {
+      mql.addEventListener('change', handler);
+    };
+  }, [query]);
+
+  return matches;
+}
--- src/shared/lib/index.ts
+++ src/shared/lib/index.ts
@@ -1,3 +1,4 @@
 export { getTransition } from './styles/get-transition';
 export { getRems } from './styles/get-rems';
 export { createMq } from './styles/create-mq';
+export { useMediaQuery } from './hooks';

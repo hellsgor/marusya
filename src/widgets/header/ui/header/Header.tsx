6931683af49a3cef821d7e0e7e013f39ae13c7b7
Igor Storchevoy <hellsgor@gmail.com>
2025-09-09 01:32:54 +0400

    Update[Header]: added user icon for menu item adaptability

--- src/widgets/header/ui/header/Header.tsx
+++ src/widgets/header/ui/header/Header.tsx
@@ -1,8 +1,11 @@
-import { Container, Logo, MenuItem } from '@/shared/ui';
+import { Container, Icon, Logo, MenuItem } from '@/shared/ui';
 import * as S from './Header.styled';
 import { MainMenu } from '../main-menu';
+import { useMediaQuery } from '@/shared/lib';
 
 export function Header() {
+  const isIconViewed = useMediaQuery('lg');
+
   return (
     <header>
       <Container>
@@ -11,7 +14,10 @@ export function Header() {
             <Logo />
           </S.StyledLogo>
           <MainMenu />
-          <MenuItem href="some" children="Войти" />
+          <MenuItem
+            href="some"
+            children={isIconViewed ? <Icon.User /> : 'Войти'}
+          />
         </S.StyledWrapper>
       </Container>
     </header>

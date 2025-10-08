5f01b75cca141da0511fec11dd7d18aaed7aa4ed
Igor Storchevoy <hellsgor@gmail.com>
2025-10-08 23:29:02 +0400

    format[shared/config/styles]: format fonts styles rules order

--- src/shared/config/styles/fonts.ts
+++ src/shared/config/styles/fonts.ts
@@ -3,17 +3,17 @@ import { css } from 'styled-components';
 export const fonts = css`
   @font-face {
     font-family: Play;
-    src: url('/fonts/Play-Regular.woff2') format('woff2');
     font-weight: 400;
     font-style: normal;
     font-display: swap;
+    src: url('/fonts/Play-Regular.woff2') format('woff2');
   }
 
   @font-face {
     font-family: Play;
-    src: url('/fonts/Play-Bold.woff2') format('woff2');
     font-weight: 700;
     font-style: normal;
     font-display: swap;
+    src: url('/fonts/Play-Bold.woff2') format('woff2');
   }
 `;

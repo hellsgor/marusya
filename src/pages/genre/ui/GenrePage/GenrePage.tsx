20d5d5bd0fe1a9b3b6445be8b3e96d2fb63fe841
Igor Storchevoy <hellsgor@gmail.com>
2025-10-20 00:25:47 +0400

    feat[genre-page]: implement GenrePage component with genre-based movie fetching

--- src/pages/genre/ui/GenrePage/GenrePage.tsx
+++ src/pages/genre/ui/GenrePage/GenrePage.tsx
@@ -1,3 +1,40 @@
+import * as S from './GenrePage.styled';
+
+import { useGetByGenreQuery } from '@/entities/movie';
+import { skipToken } from '@reduxjs/toolkit/query';
+
+import { useState } from 'react';
+import { useParams } from 'react-router';
+
+import { sortMoviesByRating } from '../../lib/sortMoviesByRating';
+import { MOVIES_PER_PAGE } from '../../config/constants';
+import { GENRES_RU } from '@/entities/genre/config/genresRu';
+
+import { BackTitleBar, Section } from '@/shared/ui';
+import { capitalizeFirstLetter } from '@/shared/lib';
+
 export function GenrePage() {
-  return <div>GenrePage</div>;
+  const [page, setPage] = useState(1);
+  const { genre } = useParams();
+  const { data } = useGetByGenreQuery(
+    genre
+      ? { genre, page, sortFunc: sortMoviesByRating, count: MOVIES_PER_PAGE }
+      : skipToken,
+  );
+
+  return (
+    <Section indents={'160px'}>
+      <S.StyledWrapper>
+        <BackTitleBar>
+          <h1>{capitalizeFirstLetter(GENRES_RU[genre ?? ''])}</h1>
+        </BackTitleBar>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus
+          et tempora dolorem necessitatibus quas natus officia vitae ad
+          consequatur obcaecati. Ipsam iusto fugit reiciendis libero sint, aut
+          id. Quas, quisquam.
+        </p>
+      </S.StyledWrapper>
+    </Section>
+  );
 }

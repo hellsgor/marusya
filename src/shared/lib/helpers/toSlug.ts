51bd9886eb2090072f7f4f506e550234cad0706b
Igor Storchevoy <hellsgor@gmail.com>
2025-10-23 02:49:34 +0400

    feat[router]: created toSlug function

---
+++ src/shared/lib/helpers/toSlug.ts
@@ -0,0 +1,50 @@
+const map: Record<string, string> = {
+  а: 'a',
+  б: 'b',
+  в: 'v',
+  г: 'g',
+  д: 'd',
+  е: 'e',
+  ё: 'e',
+  ж: 'zh',
+  з: 'z',
+  и: 'i',
+  й: 'i',
+  к: 'k',
+  л: 'l',
+  м: 'm',
+  н: 'n',
+  о: 'o',
+  п: 'p',
+  р: 'r',
+  с: 's',
+  т: 't',
+  у: 'u',
+  ф: 'f',
+  х: 'h',
+  ц: 'c',
+  ч: 'ch',
+  ш: 'sh',
+  щ: 'sch',
+  ъ: '',
+  ы: 'y',
+  ь: '',
+  э: 'e',
+  ю: 'yu',
+  я: 'ya',
+};
+
+export function toSlug(input: string): string {
+  return input
+    .split('')
+    .map((ch) => {
+      const lower = ch.toLowerCase();
+      const repl = map[lower];
+      return repl ? (ch === lower ? repl : repl.toUpperCase()) : ch;
+    })
+    .join('')
+    .toLowerCase()
+    .replace(/[^a-z0-9]+/g, '-')
+    .replace(/^-+|-+$/g, '')
+    .replace(/-{2,}/g, '-');
+}
--- src/shared/lib/index.ts
+++ src/shared/lib/index.ts
@@ -1,3 +1,3 @@
 export { createMq, getRems, getTransition } from './styles';
 export { useMediaQuery } from './hooks';
-export { capitalizeFirstLetter } from './helpers';
+export { capitalizeFirstLetter, toSlug } from './helpers';

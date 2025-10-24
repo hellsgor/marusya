d82dd300204c3950b18c0fb2ad30f5bf0567fefe
Igor Storchevoy <hellsgor@gmail.com>
2025-10-25 01:26:39 +0400

    feat[rating]: add Rating component

--- src/entities/movie/index.ts
+++ src/entities/movie/index.ts
@@ -1,3 +1,4 @@
 export { useGetByGenreQuery, useGetByIdQuery, movieApi } from './api/movieApi';
 export type { MoviesModel, MovieModel } from './model/types';
 export { MovieCard } from './ui/movie-card';
+export { Rating } from './ui/rating';

5f8c8edc10b61c31cea35dd805b19afbd774a96f
Igor Storchevoy <hellsgor@gmail.com>
2025-10-30 00:07:58 +0400

    fix[shared/ui/logo]: refactor Logo component to destructure props

--- src/shared/ui/logo/Logo.tsx
+++ src/shared/ui/logo/Logo.tsx
@@ -2,8 +2,9 @@ import * as S from './Logo.styled';
 import type { LogoProps } from './types';
 
 export function Logo(props: LogoProps) {
+  const { logoTheme, ...restProps } = props;
   return (
-    <S.StyledLogo {...props} $logoTheme={props.logoTheme || 'dark'}>
+    <S.StyledLogo {...restProps} $logoTheme={logoTheme || 'dark'}>
       <S.LogoImage src="/images/marusya-image.png" alt="Marusya logo image" />
       <S.LogoTextIcon />
     </S.StyledLogo>

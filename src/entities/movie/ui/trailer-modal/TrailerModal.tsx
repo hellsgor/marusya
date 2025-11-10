44d387508b1e637a9b8b253edad0c1ccc98f3dbf
Igor Storchevoy <hellsgor@gmail.com>
2025-11-11 02:03:54 +0400

    feat[entities/movie]: implement trailer modal with react-player

--- src/entities/movie/ui/trailer-modal/TrailerModal.tsx
+++ src/entities/movie/ui/trailer-modal/TrailerModal.tsx
@@ -1,5 +1,7 @@
 import { Modal } from '@/shared/ui';
 import type { MovieModel } from '../../model/types';
+import ReactPlayer from 'react-player';
+import { useRef } from 'react';
 
 type TrailerModalProps = {
   isVisible: boolean;
@@ -8,12 +10,38 @@ type TrailerModalProps = {
 };
 
 export function TrailerModal({ isVisible, onClose, movie }: TrailerModalProps) {
+  const playerRef = useRef<HTMLVideoElement | null>(null);
+
+  const handleOnClose = () => {
+    onClose();
+    if (playerRef.current) {
+      playerRef.current.currentTime = 0;
+    }
+  };
+
   return (
     <Modal
       type="trailer"
       isVisible={isVisible}
-      onClose={onClose}
+      onClose={handleOnClose}
       name="trailer"
-    />
+    >
+      <ReactPlayer
+        ref={playerRef}
+        playing={isVisible}
+        src={movie.trailerUrl ?? undefined}
+        width={'100%'}
+        height={'100%'}
+        controls
+        config={{
+          youtube: {
+            rel: 0,
+            iv_load_policy: 3,
+            fs: 0,
+            enablejsapi: 1,
+          },
+        }}
+      />
+    </Modal>
   );
 }

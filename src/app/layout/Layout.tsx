8d27c9d82969653d1085ad8caca5b67c02b14737
Igor Storchevoy <hellsgor@gmail.com>
2025-11-15 00:42:09 +0400

    refactor[app/layout]: migrate layout to scss modules

--- src/app/layout/Layout.tsx
+++ src/app/layout/Layout.tsx
@@ -1,21 +1,24 @@
-import { Header } from '@/widgets/header';
-import { Footer } from '@/widgets/footer';
+import s from './layout.module.scss';
+
+// import { Header } from '@/widgets/header';
+// import { Footer } from '@/widgets/footer';
 import { Outlet } from 'react-router';
-import * as S from './Layout.styled';
-import { Suspense } from 'react';
-import { Loader, ScrollToTop } from '@/shared/ui';
+// import * as S from './Layout.styled';
+// import { Suspense } from 'react';
+// import { Loader, ScrollToTop } from '@/shared/ui';
+import { ScrollToTop } from '@/shared/ui';
 
 export function Layout() {
   return (
     <>
       <ScrollToTop />
-      <Header />
-      <S.StyledMain>
-        <Suspense fallback={<Loader size="big" fixed />}>
-          <Outlet />
-        </Suspense>
-      </S.StyledMain>
-      <Footer />
+      {/* <Header /> */}
+      <main className={s.main}>
+        {/* <Suspense fallback={<Loader size="big" fixed />}> */}
+        <Outlet />
+        {/* </Suspense> */}
+      </main>
+      {/* <Footer /> */}
       <div id="modals"></div>
     </>
   );

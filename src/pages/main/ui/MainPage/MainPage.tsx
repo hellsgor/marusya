1bd194f11453871d974d09815d53ae0b9121ff1c
Igor Storchevoy <hellsgor@gmail.com>
2025-11-15 00:42:18 +0400

    refactor[pages/main]: temporary test content

--- src/pages/main/ui/MainPage/MainPage.tsx
+++ src/pages/main/ui/MainPage/MainPage.tsx
@@ -1,15 +1,15 @@
 import { useGetRandomQuery } from '@/entities/movie';
 import { useMediaQuery } from '@/shared/lib';
-import { Loader, Section, PageError } from '@/shared/ui';
-import { MovieDetailContainer } from '@/widgets/movie-detail-container';
+// import { Loader, Section, PageError } from '@/shared/ui';
+// import { MovieDetailContainer } from '@/widgets/movie-detail-container';
 
 export function MainPage() {
-  const mq = useMediaQuery('md');
-  const { data: movie, isFetching, isError, refetch } = useGetRandomQuery();
+  // const mq = useMediaQuery('md');
+  // const { data: movie, isFetching, isError, refetch } = useGetRandomQuery();
 
   return (
     <>
-      <Section indents={[!mq ? '128px' : '64px', !mq ? '0' : '24px']}>
+      {/* <Section indents={[!mq ? '128px' : '64px', !mq ? '0' : '24px']}>
         {isFetching && <Loader size="big" fixed />}
         {isError && (
           <PageError errorCode="e001" backdropText="Ooops!"></PageError>
@@ -23,7 +23,11 @@ export function MainPage() {
             }}
           />
         )}
-      </Section>
+      </Section> */}
+      <h1>hello</h1>
+      <h2>hello</h2>
+      <h3>hello</h3>
+      <p>hello</p>
     </>
   );
 }

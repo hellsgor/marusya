2705ce5ddd08cf5649de9c021a6c88e8e7e9ff4e
Igor Storchevoy <hellsgor@gmail.com>
2025-11-16 18:04:29 +0400

    refactor[styles]: update SCSS structure and add node types

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import path from 'path';
 
 export default defineConfig({
   server: {
@@ -10,6 +11,16 @@ export default defineConfig({
   plugins: [react(), svgr(), tsconfigPaths()],
   css: {
     devSourcemap: true,
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler',
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
   build: {
     rollupOptions: {

4df12e5b28ba6a698b62aac60a885ad91c541a03
Igor Storchevoy <hellsgor@gmail.com>
2025-11-18 23:24:18 +0400

    refactor[shared/ui/section]: migrate Section to SCSS

--- src/shared/ui/section/Section.tsx
+++ src/shared/ui/section/Section.tsx
@@ -1,6 +1,8 @@
+import s from './Section.module.scss';
+
 import type { ReactNode } from 'react';
 import { Container } from '../container';
-import * as S from './Section.styled';
+import clsx from 'clsx';
 
 export type SectionProps = {
   className?: string;
@@ -8,12 +10,23 @@ export type SectionProps = {
   indents?: string | [string, string];
 };
 
+const paddings = (indents: SectionProps['indents']) => {
+  if (!indents) return {};
+  return {
+    paddingTop: indents[0] ?? indents,
+    paddingBottom: indents[1] ?? indents[0] ?? indents,
+  };
+};
+
 export function Section({ className, children, indents }: SectionProps) {
   return (
-    <S.StyledSection className={className} $indents={indents}>
+    <section
+      className={clsx(s.section, className)}
+      style={{ ...paddings(indents) }}
+    >
       <Container>
-        <S.StyledSectionWrapper>{children}</S.StyledSectionWrapper>
+        <div className={s.section__wrapper}>{children}</div>
       </Container>
-    </S.StyledSection>
+    </section>
   );
 }

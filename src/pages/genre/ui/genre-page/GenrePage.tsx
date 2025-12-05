f0187cb74c983591fc80080543f923787734df7a
Igor Storchevoy <hellsgor@gmail.com>
2025-12-05 10:23:08 +0400

    fix[GenrePage]: fixed with pixel perfect

--- src/pages/genre/ui/genre-page/GenrePage.tsx
+++ src/pages/genre/ui/genre-page/GenrePage.tsx
@@ -1,15 +1,19 @@
 import s from './GenrePage.module.scss';
 import { useParams } from 'react-router';
 import { BackTitleBar, Section } from '@/shared/ui';
-import { capitalizeFirstLetter } from '@/shared/lib';
+import { capitalizeFirstLetter, useMediaQuery } from '@/shared/lib';
 import { GenreMoviesList } from '../genre-movies-list/GenreMoviesList';
 import { getRuGenreName } from '@/entities/genre';
 
 export function GenrePage() {
+  const mq = useMediaQuery('md');
   const { genre } = useParams();
 
   return (
-    <Section indents={['0', '160px']} className={s.genrePage}>
+    <Section
+      indents={[mq ? '24px' : '32px', mq ? '40px' : '160px']}
+      className={s.genrePage}
+    >
       <div className={s.genrePage__wrapper}>
         <BackTitleBar>
           <h1>{genre ? capitalizeFirstLetter(getRuGenreName(genre)) : ''}</h1>

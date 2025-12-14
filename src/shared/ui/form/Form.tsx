e925bc1cb3076e1c0c78c5eb4cd29d5db4cc622a
Igor Storchevoy <hellsgor@gmail.com>
2025-12-14 19:15:12 +0400

    refactor[Form]: integrate react-hook-form

--- src/shared/ui/form/Form.tsx
+++ src/shared/ui/form/Form.tsx
@@ -1,10 +1,28 @@
 import type { ReactNode } from 'react';
 import s from './Form.module.scss';
+import {
+  FormProvider,
+  useForm,
+  type FieldValues,
+  type SubmitHandler,
+} from 'react-hook-form';
 
 type FormProps = {
   children: ReactNode;
 };
 
-export function Form({ children }: FormProps) {
-  return <form className={s.form}>{children}</form>;
+export function Form<T extends FieldValues>({ children }: FormProps) {
+  const methods = useForm<T>({ shouldFocusError: false });
+  const { handleSubmit } = methods;
+
+  const submit: SubmitHandler<T> = (data) => {
+    console.log(data);
+  };
+  return (
+    <FormProvider {...methods}>
+      <form className={s.form} onSubmit={handleSubmit(submit)} noValidate>
+        {children}
+      </form>
+    </FormProvider>
+  );
 }

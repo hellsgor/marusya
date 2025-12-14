98c6b5fc952871f7d66b6ccfadf170659031de2b
Igor Storchevoy <hellsgor@gmail.com>
2025-12-14 19:15:12 +0400

    refactor[InputText]: add forwardref for react-hook-form compatibility

--- src/shared/ui/inputs/input-password/InputPassword.tsx
+++ src/shared/ui/inputs/input-password/InputPassword.tsx
@@ -1,32 +1,35 @@
 import { Icon } from '@/shared/ui/icon';
 import { InputText } from '../input-text';
 import type { InputProps } from '../input-text';
-import { useState, type InputHTMLAttributes } from 'react';
+import { useState, forwardRef, type InputHTMLAttributes } from 'react';
 
 interface InputPasswordProps
   extends Omit<InputHTMLAttributes<HTMLInputElement>, 'type'>,
     Pick<InputProps, 'error'> {}
 
-export function InputPassword({ error, ...rest }: InputPasswordProps) {
-  const [type, setType] = useState<'text' | 'password'>('password');
-  const eyeButton = (
-    <button
-      type="button"
-      onClick={() =>
-        setType((state) => (state === 'text' ? 'password' : 'text'))
-      }
-    >
-      {type === 'password' ? <Icon.EyeSlash /> : <Icon.Eye />}
-    </button>
-  );
+export const InputPassword = forwardRef<HTMLInputElement, InputPasswordProps>(
+  ({ error, ...rest }, ref) => {
+    const [type, setType] = useState<'text' | 'password'>('password');
+    const eyeButton = (
+      <button
+        type="button"
+        onClick={() =>
+          setType((state) => (state === 'text' ? 'password' : 'text'))
+        }
+      >
+        {type === 'password' ? <Icon.EyeSlash /> : <Icon.Eye />}
+      </button>
+    );
 
-  return (
-    <InputText
-      type={type}
-      error={error}
-      icon="Password"
-      rightSlotChild={eyeButton}
-      {...rest}
-    />
-  );
-}
+    return (
+      <InputText
+        ref={ref}
+        type={type}
+        error={error}
+        icon="Password"
+        rightSlotChild={eyeButton}
+        {...rest}
+      />
+    );
+  },
+);
--- src/shared/ui/inputs/input-text/InputText.tsx
+++ src/shared/ui/inputs/input-text/InputText.tsx
@@ -3,77 +3,80 @@ import clsx from 'clsx';
 
 import type { InputProps } from './InputText.types';
 
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState, forwardRef } from 'react';
 
 import { Icon } from '@/shared/ui/icon';
 import { getCSSNumericValue } from '@/shared/lib';
 
-export function InputText({
-  type = 'text',
-  icon,
-  error,
-  rightSlotChild,
-  ...rest
-}: InputProps) {
-  const [displayError, setDisplayError] = useState(error);
+export const InputText = forwardRef<HTMLInputElement, InputProps>(
+  ({ type = 'text', icon, error, rightSlotChild, ...rest }, ref) => {
+    const [displayError, setDisplayError] = useState(error);
 
-  const errorWrapperRef = useRef<HTMLDivElement>(null);
-  const errorTextRef = useRef<HTMLSpanElement>(null);
+    const errorWrapperRef = useRef<HTMLDivElement>(null);
+    const errorTextRef = useRef<HTMLSpanElement>(null);
 
-  const IconComponent = icon ? Icon[icon] : null;
+    const IconComponent = icon ? Icon[icon] : null;
 
-  useEffect(() => {
-    if (!errorWrapperRef.current || !errorTextRef.current) return;
+    useEffect(() => {
+      if (!errorWrapperRef.current || !errorTextRef.current) return;
 
-    const wrapperTransition =
-      getCSSNumericValue({
-        el: errorWrapperRef.current,
-        property: 'transitionDuration',
-      }) +
-      getCSSNumericValue({
-        el: errorWrapperRef.current,
-        property: 'transitionDelay',
-      });
+      const wrapperTransition =
+        getCSSNumericValue({
+          el: errorWrapperRef.current,
+          property: 'transitionDuration',
+        }) +
+        getCSSNumericValue({
+          el: errorWrapperRef.current,
+          property: 'transitionDelay',
+        });
 
-    const textTransition =
-      getCSSNumericValue({
-        el: errorTextRef.current,
-        property: 'transitionDuration',
-      }) +
-      getCSSNumericValue({
-        el: errorTextRef.current,
-        property: 'transitionDelay',
-      });
+      const textTransition =
+        getCSSNumericValue({
+          el: errorTextRef.current,
+          property: 'transitionDuration',
+        }) +
+        getCSSNumericValue({
+          el: errorTextRef.current,
+          property: 'transitionDelay',
+        });
 
-    const delay =
-      wrapperTransition > textTransition ? wrapperTransition : textTransition;
+      const delay =
+        wrapperTransition > textTransition ? wrapperTransition : textTransition;
 
-    if (error) {
-      setDisplayError(error);
-    } else {
-      const timer = setTimeout(() => setDisplayError(''), delay);
-      return () => clearTimeout(timer);
-    }
-  }, [error]);
+      if (error) {
+        setDisplayError(error);
+      } else {
+        const timer = setTimeout(() => setDisplayError(''), delay);
+        return () => clearTimeout(timer);
+      }
+    }, [error]);
 
-  return (
-    <div className={clsx(s.inputText, error && s.inputText_hasError)}>
-      <div className={s.inputText__inner}>
-        {IconComponent && (
-          <div className={s.inputText__leftSlot}>
-            <IconComponent />
+    return (
+      <div className={clsx(s.inputText, error && s.inputText_hasError)}>
+        <div className={s.inputText__inner}>
+          {IconComponent && (
+            <div className={s.inputText__leftSlot}>
+              <IconComponent />
+            </div>
+          )}
+          <input
+            ref={ref}
+            type={type}
+            className={s.inputText__input}
+            {...rest}
+          />
+          {rightSlotChild && (
+            <div className={s.inputText__rightSlot}>{rightSlotChild}</div>
+          )}
+        </div>
+        <div className={s.inputText__error}>
+          <div ref={errorWrapperRef}>
+            <span ref={errorTextRef}>{displayError ?? ''}</span>
           </div>
-        )}
-        <input type={type} {...rest} className={s.inputText__input} />
-        {rightSlotChild && (
-          <div className={s.inputText__rightSlot}>{rightSlotChild}</div>
-        )}
-      </div>
-      <div className={s.inputText__error}>
-        <div ref={errorWrapperRef}>
-          <span ref={errorTextRef}>{displayError ?? ''}</span>
         </div>
       </div>
-    </div>
-  );
-}
+    );
+  },
+);
+
+InputText.displayName = 'InputText';

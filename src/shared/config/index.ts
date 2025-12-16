e0613feeb64741a80bf2ff3c8fd7394942cb163a
Igor Storchevoy <hellsgor@gmail.com>
2025-12-16 23:51:48 +0400

    feat[config]: add validation constants and error messages

--- src/shared/config/index.ts
+++ src/shared/config/index.ts
@@ -2,3 +2,4 @@ export { ENV_CONFIG } from './env';
 export { ERRORS } from './errors';
 export { BREAKPOINTS } from './breakpoints';
 export { REGEXP } from './regexp';
+export { MIN_INPUT_LENGTH, MIN_PASSWORD_LENGTH } from './constants';

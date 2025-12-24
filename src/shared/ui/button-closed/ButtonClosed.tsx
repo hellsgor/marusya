8df6ce3b19042328b6af4f0cf792f2cf7727704c
Igor Storchevoy <hellsgor@gmail.com>
2025-12-24 23:06:59 +0400

    feat[ButtonClosed]: add onClick and disabled props

--- src/shared/ui/button-closed/ButtonClosed.tsx
+++ src/shared/ui/button-closed/ButtonClosed.tsx
@@ -1,21 +1,23 @@
 import s from './ButtonClosed.module.scss';
 import clsx from 'clsx';
 
-import { forwardRef } from 'react';
+import { forwardRef, type ButtonHTMLAttributes } from 'react';
 
 import { Icon } from '../icon';
 
 type ButtonClosedProps = {
   className?: string;
-};
+} & Pick<ButtonHTMLAttributes<HTMLButtonElement>, 'onClick' | 'disabled'>;
 
 export const ButtonClosed = forwardRef<HTMLButtonElement, ButtonClosedProps>(
-  ({ className }, ref) => {
+  ({ className, onClick, disabled }, ref) => {
     return (
       <button
         className={clsx(s.buttonClosed, className)}
         type="button"
         ref={ref}
+        onClick={onClick}
+        disabled={disabled}
       >
         <Icon.Cross />
       </button>

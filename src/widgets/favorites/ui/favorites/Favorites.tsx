3ae7828d5fb1aeb7c2fb9ddc32cfba650c2d4047
Igor Storchevoy <hellsgor@gmail.com>
2025-12-24 23:07:40 +0400

    refactor[Favorites]: use FavoriteMovieCard composition

--- src/widgets/favorites/ui/favorites/Favorites.tsx
+++ src/widgets/favorites/ui/favorites/Favorites.tsx
@@ -2,18 +2,21 @@ import { MovieList, MovieSlider } from '@/entities/movie';
 import { useGetFavoritesQuery } from '@/features/favorites';
 import { useMediaQuery } from '@/shared/lib';
 import { ErrorText, Loader } from '@/shared/ui';
+import { FavoriteMovieCard } from '../favorite-movie-card';
 
 export function Favorites() {
-  const { data, isFetching, isError } = useGetFavoritesQuery();
   const isMobile = useMediaQuery('md');
+  const { data, isLoading, isError } = useGetFavoritesQuery();
 
-  if (isFetching) return <Loader size="big" fixed />;
+  if (isLoading) return <Loader size="big" fixed />;
 
   if (isError || !data) return <ErrorText errorCode="e001" />;
 
   if (!data.length) return <ErrorText errorCode="e013" />;
 
-  if (isMobile) return <MovieSlider items={data} />;
-
-  return <MovieList items={data} />;
+  return isMobile ? (
+    <MovieSlider items={data} CardComponent={FavoriteMovieCard} />
+  ) : (
+    <MovieList items={data} CardComponent={FavoriteMovieCard} />
+  );
 }

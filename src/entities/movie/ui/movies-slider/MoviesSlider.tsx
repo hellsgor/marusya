a0313054501236b51167dd9371ca439437a5973a
Igor Storchevoy <hellsgor@gmail.com>
2025-12-30 00:19:56 +0400

    update[MoviesSlider]: optimized swiper

--- src/entities/movie/ui/movies-slider/MoviesSlider.tsx
+++ src/entities/movie/ui/movies-slider/MoviesSlider.tsx
@@ -29,10 +29,33 @@ export function MoviesSlider({
       spaceBetween={40}
       slidesPerView={'auto'}
       grabCursor={true}
-      freeMode={true}
+      freeMode={{
+        enabled: true,
+        momentum: true,
+        momentumRatio: 1.2,
+        momentumVelocityRatio: 1.1,
+        sticky: false,
+        momentumBounce: true,
+        momentumBounceRatio: 1,
+        minimumVelocity: 0.02,
+      }}
       modules={[FreeMode]}
       wrapperClass={s.moviesSlider__wrapper}
       wrapperTag="ul"
+      touchStartPreventDefault={false}
+      touchMoveStopPropagation={true}
+      touchReleaseOnEdges={true}
+      threshold={0}
+      touchAngle={45}
+      resistanceRatio={0.85}
+      shortSwipes={true}
+      longSwipes={true}
+      longSwipesRatio={0.3}
+      longSwipesMs={150}
+      followFinger={true}
+      touchRatio={1}
+      passiveListeners={false}
+      updateOnWindowResize={true}
     >
       {items.map(({ id, posterUrl, title }, idx) => (
         <SwiperSlide key={id} className={s.moviesSlider__slide} tag="li">

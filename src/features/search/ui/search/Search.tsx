766c48cb04b8cdeef7453c13ad4ce5f78b5dce00
Igor Storchevoy <hellsgor@gmail.com>
2026-01-02 09:09:07 +0400

    feat[InputSearch]: add clear button

--- src/features/search/ui/search/Search.tsx
+++ src/features/search/ui/search/Search.tsx
@@ -1,11 +1,25 @@
+import { useState } from 'react';
 import s from './Search.module.scss';
 
 import { InputSearch } from '@/shared/ui';
 
 export function Search() {
+  const [value, setValue] = useState('');
+
   return (
     <div className={s.search}>
-      <InputSearch className={s.search__input} isDark />
+      <InputSearch
+        className={s.search__input}
+        name="search"
+        value={value}
+        onClear={() => {
+          setValue('');
+        }}
+        onChange={(e) => {
+          setValue(e.target.value);
+        }}
+        isDark
+      />
     </div>
   );
 }

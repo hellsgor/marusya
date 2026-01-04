2b06e3a9f88a0eba22a3f47ec1fd405b9f8fe6bb
Igor Storchevoy <hellsgor@gmail.com>
2026-01-04 16:11:19 +0400

    fix[Search]: fix import path for MoviesModel in search types

--- src/features/search/model/types.ts
+++ src/features/search/model/types.ts
@@ -1,5 +1,5 @@
 import { MoviesSchema } from '@/entities/movie';
-import type { MoviesModel } from '@/entities/movie/model/types';
+import type { MoviesModel } from '@/entities/movie';
 import { z } from 'zod/v4';
 
 export const SearchSchema = z.object({

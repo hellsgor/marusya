61ec973f9fbebbe5f79df664ae7278440a868ea1
Igor Storchevoy <hellsgor@gmail.com>
2026-01-04 16:06:46 +0400

    feat[Search]: add search result component

--- src/features/search/ui/results-list/ResultsList.tsx
+++ src/features/search/ui/results-list/ResultsList.tsx
@@ -2,6 +2,7 @@ import s from './ResultsList.module.scss';
 import clsx from 'clsx';
 
 import type { MoviesModel } from '@/entities/movie';
+import { SearchResult } from '../search-result';
 
 type ResultsListProps = {
   className?: string;
@@ -12,9 +13,9 @@ export function ResultsList({ results, className }: ResultsListProps) {
   return (
     <ul className={clsx(s.searchResultsList, className)} role="list">
       {results?.length &&
-        results.map((result) => (
-          <li className={s.searchResultsList__result} key={result.id}>
-            {result.title}
+        results.map((movie) => (
+          <li className={s.searchResultsList__result} key={movie.id}>
+            <SearchResult data={movie} />
           </li>
         ))}
     </ul>

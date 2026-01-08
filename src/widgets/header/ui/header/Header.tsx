b0d14037e70d0bf415f0710f0431e12ad4524b8a
Igor Storchevoy <hellsgor@gmail.com>
2026-01-08 13:49:14 +0400

    fix[Header]: removed unused code

--- src/widgets/header/ui/header/Header.tsx
+++ src/widgets/header/ui/header/Header.tsx
@@ -11,47 +11,6 @@ import { useIsScrolled } from '@/shared/lib';
 import { Container } from '@/shared/ui';
 
 export function Header() {
-  // const prevScroll = useRef(0);
-  // const negativeScrollAcc = useRef(0);
-
-  // const [isScrolled, setIsScrolled] = useState(false);
-
-  // useLayoutEffect(() => {
-  //   setIsScrolled(window.scrollY >= HIDE_HEADER_SCROLL_VALUE);
-  //   prevScroll.current = window.scrollY;
-
-  //   let rafId: number | null = null;
-  //   let isScheduled = false;
-
-  //   const handleScroll = () => {
-  //     if (isScheduled) return;
-
-  //     isScheduled = true;
-
-  //     rafId = requestAnimationFrame(() => {
-  //       const delta = window.scrollY - prevScroll.current;
-  //       prevScroll.current = window.scrollY;
-
-  //       negativeScrollAcc.current =
-  //         delta < 0 ? negativeScrollAcc.current + delta : 0;
-
-  //       const shouldBeHidden =
-  //         window.scrollY >= HIDE_HEADER_SCROLL_VALUE &&
-  //         negativeScrollAcc.current >= SHOW_HEADER_SCROLL_BY;
-
-  //       setIsScrolled(shouldBeHidden);
-  //       isScheduled = false;
-  //     });
-  //   };
-
-  //   window.addEventListener('scroll', handleScroll, { passive: true });
-
-  //   return () => {
-  //     window.removeEventListener('scroll', handleScroll);
-  //     if (rafId !== null) cancelAnimationFrame(rafId);
-  //   };
-  // }, []);
-
   const isScrolled = useIsScrolled(
     HIDE_HEADER_SCROLL_VALUE,
     SHOW_HEADER_SCROLL_BY,

50dc4f90cb0ec9550f7c9b23520f7339d8bed662
Igor Storchevoy <hellsgor@gmail.com>
2026-01-08 22:48:42 +0400

    fix[ProfilePage]: adjust section indents

--- src/pages/profile/ui/profile-page/ProfilePage.tsx
+++ src/pages/profile/ui/profile-page/ProfilePage.tsx
@@ -1,9 +1,12 @@
 import { Section } from '@/shared/ui';
 import { ProfileViews } from '../profile-views/ProfileViews';
+import { useMediaQuery } from '@/shared/lib';
 
 export function ProfilePage() {
+  const mq = useMediaQuery('md');
+
   return (
-    <Section indents={['32px', '160px']}>
+    <Section indents={[mq ? '24px' : '32px', mq ? '24px' : '160px']}>
       <h1>Мой аккаунт</h1>
       <ProfileViews />
     </Section>

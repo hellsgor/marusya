15442a2f4c1dde20b14a86da631e721b79197c03
Igor Storchevoy <hellsgor@gmail.com>
2026-01-14 23:12:52 +0400

    update[api]: enhance API slice with CORS support and custom fetch function

--- src/shared/api/apiSlice.ts
+++ src/shared/api/apiSlice.ts
@@ -7,9 +7,18 @@ export const api = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: ENV_CONFIG.API_URL,
     credentials: 'include',
+    mode: 'cors',
     prepareHeaders: (headers) => {
       headers.set('Content-Type', 'application/json');
       return headers;
+    },
+    fetchFn: async (input, init) => {
+      const response = await fetch(input, {
+        ...init,
+        credentials: 'include',
+        mode: 'cors',
+      });
+      return response;
     },
   }),
   tagTypes: ['genres', 'movies', 'currentMovie', 'user'],
